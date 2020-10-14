a044311cf95b9e0c553ef72e2a5df43eea6c9801
unknown <mauriceLocal@DT-13349.Unisg.ch>
2020-10-14 13:15:03 +0200

    add github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
+  router: {
+    base: '/homelab-portfolio/',
+  },
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Homelab :: Portfolio :: Maurice Oegerli',
