44cadea248f1de4befc468c13fd2c4a7df890904
unknown <mauriceLocal@DT-13349.Unisg.ch>
2020-10-16 15:27:11 +0200

    update cards

--- components/sections/OverviewSection.vue
+++ components/sections/OverviewSection.vue
@@ -2,7 +2,9 @@
   <section class="section section-2">
     <div class="overview">
       <div class="overview-box">
-        <h2>Homelab Portfolio</h2>
+        <div class="content">
+          <h2>Homelab Portfolio</h2>
+        </div>
       </div>
     </div>
   </section>
@@ -32,15 +34,40 @@ export default Vue.extend({
     padding: 50px;
     box-sizing: border-box;
 
+    @media screen and (max-width: 800px) {
+      padding: 10px;
+    }
+
     .overview-box {
       box-sizing: border-box;
-      background: linear-gradient(135deg, #ed5565 0%, #d52739 100%);
       width: 100%;
       height: 100%;
       box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
       border-radius: 5px;
-      padding: 20px;
       color: #fff;
+      position: relative;
+
+      .content {
+        border-radius: 5px;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        padding: 20px;
+        background-color: #1e1e1e;
+        position: absolute;
+      }
+
+      &:before {
+        content: '';
+        border-radius: 5px;
+        position: absolute;
+        top: -5px;
+        left: -5px;
+        right: -5px;
+        bottom: -5px;
+        background: linear-gradient(135deg, #f6ab3e 0%, #8037f6 100%);
+      }
     }
   }
 }
--- components/sections/VideoSection.vue
+++ components/sections/VideoSection.vue
@@ -2,7 +2,9 @@
   <section class="section section-3">
     <div class="overview">
       <div class="overview-box">
-        <h2>Video</h2>
+        <div class="content">
+          <h2>Video</h2>
+        </div>
       </div>
     </div>
   </section>
@@ -21,7 +23,7 @@ export default Vue.extend({
 .section-3 {
   position: sticky;
   top: 0;
-  // background: linear-gradient(to bottom, transparent, #000000c9);
+  background: linear-gradient(to bottom, transparent, #000000c9 60%);
   // backdrop-filter: blur(50px);
   z-index: 3;
 
@@ -32,15 +34,41 @@ export default Vue.extend({
     padding: 50px;
     box-sizing: border-box;
 
+    @media screen and (max-width: 800px) {
+      padding: 10px;
+    }
+
     .overview-box {
       box-sizing: border-box;
-      background: linear-gradient(135deg, #a0d468 0%, #6dae26 100%);
       width: 100%;
       height: 100%;
       box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
       border-radius: 5px;
       padding: 20px;
       color: #fff;
+      position: relative;
+
+      .content {
+        border-radius: 5px;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        padding: 20px;
+        background-color: #1e1e1e;
+        position: absolute;
+      }
+
+      &:before {
+        content: '';
+        border-radius: 5px;
+        position: absolute;
+        top: -5px;
+        left: -5px;
+        right: -5px;
+        bottom: -5px;
+        background: linear-gradient(135deg, #44e0a7 0%, #d5e969 100%);
+      }
     }
   }
 }
