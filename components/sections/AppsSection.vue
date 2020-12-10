096dc6a8b85eb3b169a493dfd6e4776314f9c040
Maurice Oegerli <maurice@oeger.li>
2020-12-10 21:44:55 +0100

    apps gallery initial

--- components/sections/AppsSection.vue
+++ components/sections/AppsSection.vue
@@ -4,7 +4,17 @@
       <div class="overview-box">
         <div class="content">
           <h2>Apps</h2>
-          <div class="app-gallery"></div>
+          <div class="app-gallery">
+            <div v-for="app in apps" :key="app.index" class="app">
+              <div class="title-container">
+                <img :src="app.thumbnail" :alt="app.title" />
+                <h3>{{ app.title }}</h3>
+              </div>
+              <div class="description-container">
+                <p>{{ app.description }}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -18,7 +28,19 @@ export default Vue.extend({
   name: 'AppsSection',
   data() {
     return {
-      apps: [{ title: 'ViewTube', thumbnail: '@/assets/logos/viewtube.svg' }],
+      apps: [
+        {
+          title: 'ViewTube',
+          thumbnail: require('@/assets/icons/viewtube.svg'),
+          description:
+            'ViewTube is an alternative YouTube frontend using the invidio.us API. It can recommend, play and search for videos. It saves your watch progress and you can subscribe to channels for them to appear in your subscription feed.',
+        },
+        {
+          title: 'Drone.io',
+          thumbnail: require('@/assets/icons/drone.png'),
+          description: 'a',
+        },
+      ],
     };
   },
 });
@@ -37,7 +59,34 @@ export default Vue.extend({
       .content {
         .app-gallery {
           height: calc(100% - 40px);
-          overflow: auto scroll;
+          overflow: hidden auto;
+          display: grid;
+          grid-template-columns: repeat(auto-fill, 50%);
+
+          @media screen and (max-width: $mobile-width) {
+            grid-template-columns: 100%;
+          }
+
+          .app {
+            display: flex;
+            flex-direction: row;
+            margin: 10px 10px 0 0;
+
+            .title-container {
+              display: flex;
+              flex-direction: column;
+              margin: 0 10px 0 0;
+
+              img {
+                width: 100px;
+                height: 100px;
+              }
+              h3 {
+              }
+            }
+            .description-container {
+            }
+          }
         }
       }
     }
